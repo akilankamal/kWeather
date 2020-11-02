4c0b2f32dc34db0704a769e16cdda6e8b84ed979
Akilan <akilankamal@live.com>
2020-11-02 21:48:38 +0530

    Creating skeleton

---
+++ kWeather/kWeather/Modules/Weather/Network/NetworkLibrary.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkLibrary.swift
+//  kWeather
+//
+//  Created by Akilan on 02/11/20.
+//  Copyright © 2020 aki. All rights reserved.
+//
+
+import Foundation
---
+++ kWeather/kWeather/Modules/Weather/Services/Protocols/LocationService.swift
@@ -0,0 +1,9 @@
+//
+//  LocationService.swift
+//  kWeather
+//
+//  Created by Akilan on 02/11/20.
+//  Copyright © 2020 aki. All rights reserved.
+//
+
+import Foundation
